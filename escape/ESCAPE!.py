a7c2bd53eed4668a8e45317a625001262d50c6a0
M-zudin <mikzudin@gmail.com>
2024-11-16 11:36:00 +0300

    Add files via upload
    альфа 0.2.0.2

--- escape/ESCAPE!.py
+++ escape/ESCAPE!.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 from time import sleep
 no_fall=None
 hasher=sha256()
-cheat='5126' #2183 отключение падения 5126 - клик тп
+cheat='5126' #2183 отключение падения 5126 - клик телепорт
 hasher.update(cheat.encode('utf-8'))
 if hasher.hexdigest()=='bc83cbb2d6dcba934deedb695609dd3ae689a72b210a8f1b86f6b1bc4c68d348':
     no_fall=1
@@ -26,20 +26,12 @@ r=(150,100,200)
 g=(0,255,0)
 d=(255,0,0)
 c=25
-a=open('levels//1.py','rb')
-lvl_data=a.read()
-a.close()
-exec(lvl_data)
-lvl_data=None
-a=open('levels//2.py','rb')
-lvl_data=a.read()
-a.close()
-exec(lvl_data)
-lvl_data=None
-com=0
-a=open('levels//3.py','rb')
-lvl_data=a.read()
-a.close()
-exec(lvl_data)
-lvl_data=None
-com=0
+def start_level(num):
+    lvl_data=None
+    a=open(f'levels//{num}.py','rb')
+    lvl_data=a.read()
+    a.close()
+    exec(lvl_data)
+start_level(1)
+start_level(2)
+start_level(3)
